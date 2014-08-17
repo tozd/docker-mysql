8a069bbe204bba2e9717e2548127205790d2340f
Mitar <mitar.git@tnode.com>
2014-08-17 13:48:04 -0700

    Renaming MySQL container.

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 mkdir -p /srv/var/log/mysql
 mkdir -p /srv/mysql
 
-docker run -d --name dbmysql -h dbmysql.mysql.server2.docker -v /srv/var/log/mysql:/var/log/mysql -v /srv/mysql:/var/lib/mysql cloyne/mysql
+docker run -d --name mysql -h mysql.mysql.server2.docker -v /srv/var/log/mysql:/var/log/mysql -v /srv/mysql:/var/lib/mysql cloyne/mysql
