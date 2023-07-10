4cb1a1fde92daae4da1b0033700ec20f1fdf4a19
Mitar <mitar.git@tnode.com>
2023-07-10 21:11:38 +0200

    Do not remove container automatically so that we can obtain logs.

--- test.sh
+++ test.sh
@@ -3,12 +3,16 @@
 set -e
 
 cleanup() {
+  echo "Logs"
+  docker logs test || true
+
   echo "Stopping Docker image"
-  docker stop test
+  docker stop test || true
+  docker rm -f test
 }
 
 echo "Running Docker image"
-docker run -d --name test --rm -p 3306:3306 "${CI_REGISTRY_IMAGE}:${TAG}"
+docker run -d --name test -p 3306:3306 "${CI_REGISTRY_IMAGE}:${TAG}"
 trap cleanup EXIT
 
 echo "Sleeping"
